31318fcf36b1d481e8d16d11939ab77808160510
Guilherme <gui07spider@gmail.com>
2023-09-26 08:22:21 -0300

    oi

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+const Tab = createBottomTabNavigator();
+import Parse from "parse/react-native.js";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+	Parse.setAsyncStorage(AsyncStorage);
+  
+  Parse.initialize('4JzJrSxrtAOcGqxiIKyy1Ueic8Kz1yE0dGrM6cb0','UnxInxzaLSrQR52fZYx34EqcSISA7vQejcBfP4H1');
+	Parse.serverURL = 'https://parseapi.back4app.com/';
 
 export default function App() {
   return (
